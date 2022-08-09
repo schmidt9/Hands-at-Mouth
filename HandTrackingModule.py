6a1ab35c359273aad0c7f1c5ac432f368c1d07be
Alexander Kormanovsky <kormanowskij@yandex.ru>
2022-08-09 20:16:36 +0300

    remove unused method

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -56,20 +56,6 @@ class HandDetector:
                 cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20), (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
         return self.lmList, bbox
 
-    def findDistance(self, p1, p2, img, draw=True):
-        x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
-        x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
-
-        if draw:
-            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
-
-        length = math.hypot(x2 - x1, y2 - y1)
-        return length, img, [x1, y1, x2, y2, cx, cy]
-
     def fingers_up(self):
         fingers = []
 
