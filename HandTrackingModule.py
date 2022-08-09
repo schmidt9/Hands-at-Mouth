0b055c0b0d8aa5c564ea29fa3fcf93a16e0a7490
Alexander Kormanovsky <kormanowskij@yandex.ru>
2022-08-09 22:07:48 +0300

    refactor, rename; add hand hull calculation;

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,7 +1,9 @@
 import cv2
 import mediapipe as mp
 import time
-import math
+
+import numpy
+from scipy.spatial import ConvexHull
 
 
 class HandDetector:
@@ -19,6 +21,8 @@ class HandDetector:
         self.mpDraw = mp.solutions.drawing_utils
         self.tipIds = [4, 8, 12, 16, 20]
 
+        self.landmarkList = []
+
     def find_hands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
@@ -28,46 +32,66 @@ class HandDetector:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
                     self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
         return img
 
     def find_position(self, img, handNo=0, draw=True):
         xList = []
         yList = []
         bbox = []
-        self.lmList = []
+        self.landmarkList = []
 
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
+            points = []
+
             for id, lm in enumerate(myHand.landmark):
                 # print(id, lm)
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 xList.append(cx)
                 yList.append(cy)
-                # print(id, cx, cy)
-                self.lmList.append([id, cx, cy])
+
+                self.landmarkList.append([id, cx, cy])
+                points.append([cx, cy])
+
                 if draw:
                     cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+
             xmin, xmax = min(xList), max(xList)
             ymin, ymax = min(yList), max(yList)
             bbox = xmin, ymin, xmax, ymax
 
             if draw:
                 cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20), (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
-        return self.lmList, bbox
+                hull = ConvexHull(points)
+
+                hull_points = []
+
+                for vertice in hull.vertices:
+                    point = points[vertice]
+                    hull_points.append(point)
+
+                hull_points_array = numpy.array(hull_points).reshape((-1, 1, 2))
+                cv2.polylines(img, [hull_points_array], True, (255, 0, 0), thickness=2)
+
+        return self.landmarkList, bbox
+
+    # def draw_boundary(self):
+
 
     def fingers_up(self):
         fingers = []
 
         # Thumb
-        if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1]:
+        if self.landmarkList[self.tipIds[0]][1] < self.landmarkList[self.tipIds[0] - 1][1]:
             fingers.append(1)
         else:
             fingers.append(0)
 
         # 4 Fingers
         for id in range(1, 5):
-            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+            if self.landmarkList[self.tipIds[id]][2] < self.landmarkList[self.tipIds[id] - 2][2]:
                 fingers.append(1)
             else:
                 fingers.append(0)
@@ -76,15 +100,16 @@ class HandDetector:
 
 def main():
     pTime = 0
-    cTime = 0
     cap = cv2.VideoCapture(0)
     detector = HandDetector()
+
     while True:
         success, img = cap.read()
         img = detector.find_hands(img)
-        lmList = detector.find_position(img)
-        if len(lmList) != 0:
-            print(lmList[1])
+        landmarkList = detector.find_position(img)
+
+        if len(landmarkList) != 0:
+            print(landmarkList[1])
 
         cTime = time.time()
         fps = 1. / (cTime - pTime)
