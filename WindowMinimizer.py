561f751bdd4afc64ce487bf89893dedc3a4d35ea
Alexander Kormanovsky <kormanowskij@yandex.ru>
2022-08-11 21:09:02 +0300

    implement WindowMinimizer action

--- WindowMinimizer.py
+++ WindowMinimizer.py
@@ -1,7 +1,12 @@
+import pyautogui
 from HandsAtMouthListener import HandsAtMouthListener
 
 
 class WindowMinimizer(HandsAtMouthListener):
 
     def execute_action(self):
-        print("ACTION")
+        try:
+            # getActiveWindow() not found on macOS
+            pyautogui.getActiveWindow().minimize()
+        except (pyautogui.PyAutoGUIException, AttributeError) as ex:
+            print(ex)
