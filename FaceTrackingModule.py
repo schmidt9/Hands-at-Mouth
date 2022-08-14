91b76a38f4de9660dfe961a5f7475c1859e548a3
Alexander Kormanovsky <kormanowskij@yandex.ru>
2022-08-14 14:27:24 +0300

    output sunglasses with alpha

--- FaceTrackingModule.py
+++ FaceTrackingModule.py
@@ -1,5 +1,7 @@
 import cv2
 import mediapipe as mp
+import numpy
+
 import GeometryHelper
 
 
@@ -70,7 +72,7 @@ class IrisesDetector(FaceDetector):
 
         return img, self.hull_points
 
-    def show_pixel_sunglasses_image(self, img):
+    def add_pixel_sunglasses_image(self, img):
         # resize sunglasses to fit eyes
         resize_factor = 2.0
 
@@ -93,16 +95,64 @@ class IrisesDetector(FaceDetector):
 
         x = int(right_iris_center.x - ((new_image_width - irises_distance) / 2))
         y = int(right_iris_center.y - (new_image_height / 2))
-        img[y:y + new_image_height,
-            x:x + new_image_width] = resized_image
+
+        img = self.__class__.add_transparent_image(img, resized_image, x, y)
 
         return img
+
+    @staticmethod
+    def add_transparent_image(background, foreground, x_offset=None, y_offset=None):
+        """
+        https://stackoverflow.com/a/71701023/3004003
+        """
+        bg_h, bg_w, bg_channels = background.shape
+        fg_h, fg_w, fg_channels = foreground.shape
+
+        assert bg_channels == 3, f'background image should have exactly 3 channels (RGB). found:{bg_channels}'
+        assert fg_channels == 4, f'foreground image should have exactly 4 channels (RGBA). found:{fg_channels}'
+
+        # center by default
+        if x_offset is None:
+            x_offset = (bg_w - fg_w) // 2
+
+        if y_offset is None:
+            y_offset = (bg_h - fg_h) // 2
+
+        w = min(fg_w, bg_w, fg_w + x_offset, bg_w - x_offset)
+        h = min(fg_h, bg_h, fg_h + y_offset, bg_h - y_offset)
+
+        if w < 1 or h < 1:
+            return background
+
+        # clip foreground and background images to the overlapping regions
+        bg_x = max(0, x_offset)
+        bg_y = max(0, y_offset)
+        fg_x = max(0, x_offset * -1)
+        fg_y = max(0, y_offset * -1)
+        foreground = foreground[fg_y:fg_y + h, fg_x:fg_x + w]
+        background_subsection = background[bg_y:bg_y + h, bg_x:bg_x + w]
+
+        # separate alpha and color channels from the foreground image
+        foreground_colors = foreground[:, :, :3]
+        alpha_channel = foreground[:, :, 3] / 255  # 0-255 => 0.0-1.0
+
+        # construct an alpha_mask that matches the image shape
+        alpha_mask = numpy.dstack((alpha_channel, alpha_channel, alpha_channel))
+
+        # combine the background with the overlay image weighted by alpha
+        composite = background_subsection * (1 - alpha_mask) + foreground_colors * alpha_mask
+
+        # overwrite the section of the background image that has been updated
+        background[bg_y:bg_y + h, bg_x:bg_x + w] = composite
+
+        return background
 
     @staticmethod
     def __read_pixel_sunglasses_image():
         image_name = "pixel_sunglasses.png"
         image_dir = "img"
-        return cv2.imread(f'{image_dir}/{image_name}')
+        # using IMREAD_UNCHANGED to read image with alpha channel
+        return cv2.imread(f'{image_dir}/{image_name}', cv2.IMREAD_UNCHANGED)
 
 
 class LipsDetector(FaceDetector):
@@ -126,7 +176,7 @@ def main():
         img = cv2.flip(img, 1)
         img, _ = lips_detector.find_face_mesh_connection(img)
         img, _ = irises_detector.find_face_mesh_connection(img)
-        img = irises_detector.show_pixel_sunglasses_image(img)
+        img = irises_detector.add_pixel_sunglasses_image(img)
         cv2.imshow("Image", img)
         cv2.waitKey(1)
 
